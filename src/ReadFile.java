f5f303f22ba34166df51e633235b05ac7bf436d8
ElifsuKahraman <136170828+ElifsuKahraman@users.noreply.github.com>
2024-08-27 11:38:45 +0300

    created readfile

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.io.File;
 
 public class ReadFile {
-}
+        File file;
+
+        public ReadFile(File file) {
+            this.file = file;
+        }
+    }
+
