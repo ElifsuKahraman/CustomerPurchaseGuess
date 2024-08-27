c84cd879467b1b75ee3ad2a8e9941446906f946d
ElifsuKahraman <136170828+ElifsuKahraman@users.noreply.github.com>
2024-08-27 11:38:31 +0300

    created main

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main (String[]args) throws Exception {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (UnsupportedLookAndFeelException e) {
+                throw new RuntimeException(e);
+            }
+
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    FormCustomerPurchase frm = new FormCustomerPurchase();
+                    frm.setVisible(true);
+                }
+            });
+        }
+    }
+
+
+
