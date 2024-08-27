7eac9e779182af067878d4ade1c76e7fcf5cc065
ElifsuKahraman <136170828+ElifsuKahraman@users.noreply.github.com>
2024-08-27 11:37:19 +0300

    created cvstoarff

--- src/CsvToArff.java
+++ src/CsvToArff.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.io.File;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
 
-public class CsvToArff {
-}
+    public class CsvToArff {
+        public CsvToArff() {
+        }
+
+        public void file() throws Exception {
+            CSVLoader loader = new CSVLoader();
+            loader.setSource(new File("C:\\Users\\kelif\\Desktop\\musteri_tahmin.csv"));
+            Instances data = loader.getDataSet();
+            data.setClassIndex(data.numAttributes() - 1);
+            ArffSaver saver = new ArffSaver();
+            saver.setInstances(data);
+            saver.setFile(new File("C:\\Users\\kelif\\Desktop\\musteri_tahmin.arff"));
+            saver.writeBatch();
+        }
+    }
+
+
