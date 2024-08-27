3eb3b89c494f685c77ed811bae989b0d94d054c3
ElifsuKahraman <136170828+ElifsuKahraman@users.noreply.github.com>
2024-08-27 17:51:22 +0300

    update creating algorithm

--- src/CreatingAlgorithm.java
+++ src/CreatingAlgorithm.java
@@ -10,9 +10,20 @@ import weka.core.Instances;
 
 public class CreatingAlgorithm {
     ReadFile readFile;
+    Instances trainingData;
+    Instances testData;
+    RandomTree randomTree;
+    Evaluation eval;
+    double kappa;
+    double accuarcy;
+    double rmse;
+
+
+
 
     public CreatingAlgorithm(ReadFile readFile) {
         this.readFile = readFile;
+
     }
 
     public void modelCreation() throws Exception {
@@ -35,25 +46,68 @@ public class CreatingAlgorithm {
         Instances dataSet = new Instances(bufferedReader);
         int trainSize = (int)Math.round((double)dataSet.numInstances() * 0.7);
         int testSize = dataSet.numInstances() - trainSize;
-        Instances trainingData = new Instances(dataSet, 0, trainSize);
+        trainingData = new Instances(dataSet, 0, trainSize);
         trainingData.setClassIndex(trainingData.numAttributes() - 1);
-        Instances testData = new Instances(dataSet, trainSize, testSize);
+        testData = new Instances(dataSet, trainSize, testSize);
         testData.setClassIndex(testData.numAttributes() - 1);
-        RandomTree randomTree = new RandomTree();
+        randomTree = new RandomTree();
         randomTree.buildClassifier(trainingData);
 
         for(int i = 0; i < testData.numInstances(); ++i) {
             double actualClass = testData.instance(i).classValue();
             String actual = testData.classAttribute().value((int)actualClass);
             Instance newInts = testData.instance(i);
-            double predNb = randomTree.classifyInstance(newInts);
-            String predString = testData.classAttribute().value((int)predNb);
+            double predRt = randomTree.classifyInstance(newInts);
+            String predString = testData.classAttribute().value((int)predRt);
             System.out.println(actual + " " + predString);
         }
-
-        Evaluation eval = new Evaluation(trainingData);
+        eval = new Evaluation(trainingData);
         eval.evaluateModel(randomTree, testData);
-        JOptionPane.showMessageDialog(null, eval.toSummaryString("\n Evaulation results: \n", false));
-        JOptionPane.showMessageDialog(null, eval.toMatrixString("=== Overall Confisuon Matrix ===\n"));
+        kappa = eval.kappa();
+        accuarcy=eval.pctCorrect();
+        rmse=eval.rootMeanSquaredError();
+
+
+        JOptionPane.showMessageDialog(null, eval.toSummaryString("\n Değerlendirme Sonuçları: \n", false));
+        JOptionPane.showMessageDialog(null, eval.toMatrixString("=== Genel Karışıklık Matrisi ===\n"));
+    }
+
+
+    public void comment() throws Exception {
+        String mesaj="";
+        JOptionPane pane=new JOptionPane();
+        if(kappa>=0.01 && kappa<=0.40  ){
+            mesaj+="Kappa: " + kappa +"\n" + "Zayıf Anlaşma";
+        }
+        else if(kappa>=0.41 && kappa<=0.60   ){
+            mesaj+="Kappa: " + kappa +"\n" + "Orta Anlaşma";
+        }
+        else if(kappa>=0.61 && kappa<=0.80 ){
+            mesaj+="Kappa: " + kappa +"\n" + "İyi Anlaşma";
+        }
+        else if(kappa>=0.81 && kappa<=1 ){
+            mesaj+="Kappa: " + kappa +"\n" + "Çok iyi Anlaşma";
+        }
+        else{
+            mesaj+="Kappa hesaplanamadı";
+        }
+
+        if(accuarcy>=70){
+            mesaj+="Correctly Classified Instances: " + accuarcy +"\n" + "Kabul edilebilir.";
+
+        }
+        else {
+            mesaj+="Correctly Classified Instances: " + accuarcy +"\n" + "Kabul edilemez.";
+
+        }
+
+        if(rmse<=100){
+            mesaj+="Root Relative Squared Error: " + rmse +"\n" + "İyi bir modele yakın.";
+
+        }
+        else{
+            mesaj+="Root Relative Squared Error: " + rmse +"\n" + "Kötü model.";
+        }
+        pane.showMessageDialog(null, mesaj);
     }
 }
